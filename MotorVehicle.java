f8ce2cc5ea7d05d7cbf147286f3a14230daa75c1
Joshua <buildingguy@sbcglobal.net>
2022-10-10 17:30:30 -0700

    create variables for MotorVehicle.java

--- MotorVehicle.java
+++ MotorVehicle.java
@@ -1,3 +1,9 @@
 public abstract class MotorVehicle {
-    
+    // variables
+    private String brakeSystemType;
+    private String emissionSystemType;
+    private String engineCoolingSystemType;
+    private String steeringType;
+    private String transmissionType;
+    private int mileage;
 }
