e4726ec41bcef91cc9cfb9d506a6589cef4b1f2e
Joshua <buildingguy@sbcglobal.net>
2022-10-10 17:45:09 -0700

    use variables from superclass

--- Car.java
+++ Car.java
@@ -4,4 +4,8 @@ public class Car extends MotorVehicle {
     private String model;
     private String color;
     private int wheels;
+
+    public Car (String brake, String emission, String engine, String steering, String transmission, int mileage) {
+        super(brake, emission, engine, steering, transmission, mileage);
+    }
 }
