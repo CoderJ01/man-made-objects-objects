2e0c48ba359542d8f27991200e295d89a6531437
Joshua <buildingguy@sbcglobal.net>
2022-10-10 18:58:37 -0700

    pseudocode

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
 public class Main {
 
     public static void main(String[] args) {
+        // Car test
         Car car = new Car("parking brake", "exhaust emissions", "air cooling", "fully electric power steering", "manuel transmission", 100000, "Honda", "Civic", "dark blue", 4);
         System.out.println(car.getColor());
         System.out.println(car.getBrakeType());
