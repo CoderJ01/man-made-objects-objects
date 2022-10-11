9bd0f373e1ad52b6f828aa9a89e7834fc3c1ade8
Joshua <buildingguy@sbcglobal.net>
2022-10-10 17:34:23 -0700

    create MotorVehicle.java constructor

--- MotorVehicle.java
+++ MotorVehicle.java
@@ -6,4 +6,14 @@ public abstract class MotorVehicle {
     private String steeringType;
     private String transmissionType;
     private int mileage;
+
+    // constructor
+    public MotorVehicle(String brake, String emission, String engine, String steering, String transmission, int mileage) {
+        this.brakeSystemType = brake;
+        this.emissionSystemType = emission;
+        this.engineCoolingSystemType = engine;
+        this.steeringType = steering;
+        this.transmissionType = transmission;
+        this.mileage = mileage;
+    }
 }
