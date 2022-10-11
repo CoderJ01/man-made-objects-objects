71afa2c0f7e8c34470be43e4474ecabbefe1ad3a
Joshua <buildingguy@sbcglobal.net>
2022-10-10 20:18:43 -0700

    clean

--- Main.java
+++ Main.java
@@ -1,30 +1,6 @@
 public class Main {
 
     public static void main(String[] args) {
-        // Car test
-        Car car = new Car("parking brake", "exhaust emissions", "air cooling", "fully electric power steering", "manuel transmission", 100000, "Honda", "Civic", "dark blue", 4);
-        System.out.println(car.getColor());
-        System.out.println(car.getBrakeType());
-        car.setColor("black");
-        System.out.println(car.getColor());
-        System.out.println("");
-
-        // House test
-        House house = new House("concrete slab", 1500, 5, true, true, true);
-        House.Bedroom bedroom = house.new Bedroom("twin-sized");
-        System.out.println(house.getFoundationType());
-        System.out.println(bedroom.getBedType());
-        bedroom.setPaintColors("royal blue");
-        bedroom.setPaintColors("dim gray");
-        System.out.println(bedroom.getPaintColors());
-        System.out.println(house.hasBasement());
-        house.setBasementArea(345);
-        System.out.println(house.getBasementArea());
-        house.setAtticArea(210);
-        System.out.println(house.getAtticArea());
-        house.setBackyardArea(500);
-        System.out.println(house.getBackyardArea());
-        
-
+       
     }
 }
