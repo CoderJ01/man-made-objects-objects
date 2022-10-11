459201e666b52ee8a38206bb88eee29f6280aeca
Joshua <buildingguy@sbcglobal.net>
2022-10-10 21:31:23 -0700

    clean

--- Main.java
+++ Main.java
@@ -1,10 +1,6 @@
 public class Main {
 
     public static void main(String[] args) {
-       IComponent house = new House("basement", 500, 2, true, false, true);
-       System.out.println(house.setNumberOfWindows(10));
-       house.setMaterials("brick");
-       house.setMaterials("wood");
-       System.out.println(house.getMaterials());
+
     }
 }
