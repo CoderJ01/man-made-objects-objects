f97f32c188dccf1edbc33a7c23519dabe1a73d43
Joshua <buildingguy@sbcglobal.net>
2022-10-10 17:47:11 -0700

    finish Car class constructor w/ Car variables

--- Car.java
+++ Car.java
@@ -5,7 +5,11 @@ public class Car extends MotorVehicle {
     private String color;
     private int wheels;
 
-    public Car (String brake, String emission, String engine, String steering, String transmission, int mileage) {
+    public Car (String brake, String emission, String engine, String steering, String transmission, int mileage, String make, String model, String color, int wheels) {
         super(brake, emission, engine, steering, transmission, mileage);
+        this.make = make;
+        this.model = model;
+        this.color = color;
+        this.wheels = wheels;
     }
 }
