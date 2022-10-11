1e03416133cfe1099afe48526a05d087aace4d80
Joshua <buildingguy@sbcglobal.net>
2022-10-10 18:37:09 -0700

    House constructor

--- House.java
+++ House.java
@@ -3,4 +3,12 @@ public class House extends Building {
     private boolean hasBasement;
     private boolean hasAttic;
     private boolean hasBackyard;
+
+    // constructor
+    public House(String foundation, int area, int rooms, boolean basement, boolean attic, boolean backyard) {
+        super(foundation, area, rooms);
+        this.hasBackyard = basement;
+        this.hasAttic = attic;
+        this.hasBackyard = backyard;
+    }
 }
