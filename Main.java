9b52076e286c02354e6dec129c50d207dce122a2
Joshua <buildingguy@sbcglobal.net>
2022-10-10 18:15:36 -0700

    setter test

--- Main.java
+++ Main.java
@@ -4,5 +4,7 @@ public class Main {
         Car car = new Car("parking brake", "exhaust emissions", "air cooling", "fully electric power steering", "manuel transmission", 100000, "Honda", "Civic", "dark blue", 4);
         System.out.println(car.getColor());
         System.out.println(car.getBrakeType());
+        car.setColor("black");
+        System.out.println(car.getColor());
     }
 }
